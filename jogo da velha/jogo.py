cd023cd84600855c2f56491d3b6c11e685d524aa
pattrickx <pattrickpx99@gmail.com>
2021-04-06 20:31:48 -0300

    melhorando interface

--- jogo da velha/jogo.py
+++ jogo da velha/jogo.py
@@ -9,8 +9,9 @@ tabuleiro = [[0,0,0],
             [0,0,0]]
 
 def desenhar_X(janela,x,y,t):
-    pygame.draw.line(janela,(255,0,0),(x*t,y*t),((x+1)*t,(y+1)*t),3)
-    pygame.draw.line(janela,(255,0,0),(x*t,(y+1)*t),((x+1)*t,(y)*t),3)
+    pygame.draw.line(janela,(255,0,0),(x*t+20,y*t+20),((x+1)*t-20,(y+1)*t-20),8)
+    pygame.draw.line(janela,(255,0,0),(x*t+20,(y+1)*t-20),((x+1)*t-20,(y)*t+20),8)
+
 def desenhar_tabuleiro(janela,tabuleiro):
     w = 600
     h = 600
@@ -23,9 +24,10 @@ def desenhar_tabuleiro(janela,tabuleiro):
     for i in range(len(tabuleiro)):
         for j in range(len(tabuleiro)):
             if tabuleiro[i][j] == -1:
-                pygame.draw.circle(janela, (0,0,255), ((i+1)*tamanho-raio, (j+1)*tamanho-raio),raio, 3)
+                pygame.draw.circle(janela, (0,0,255), ((i+1)*tamanho-raio, (j+1)*tamanho-raio),raio-20, 8)
             if tabuleiro[i][j] == 1:
                 desenhar_X(janela,i,j,tamanho)
+
 def redesenhar(janela,tabuleiro):
     janela.fill((255,255,255))
     desenhar_tabuleiro(janela,tabuleiro)
@@ -65,7 +67,6 @@ def ganhador(t):
 
     return 0
 
-        
 def main(jogador=1):
     janela = pygame.display.set_mode((600,600))
     
@@ -90,14 +91,13 @@ def main(jogador=1):
                     else:
                         tabuleiro[i][j]=-1
                         jogador=1
-                print(i,j)
-                print(tabuleiro[i][j])
-                print(jogada)
+                # print(i,j)
+                # print(tabuleiro[i][j])
+                # print(jogada)
                 g = ganhador(tabuleiro)
                 if g!=0:
                     print(f"ganhador: {g}")
-    
-    
-
+                if g==0 and jogada ==9:
+                    print("Empate")
 
 main()
